1370a7af0dc35cc86fd02c58eaa3186977f996b8
Brooke White <whbrook@amazon.com>
2021-05-10 09:09:55 -0700

    test(hooks): disable writing test failures to file

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -11,17 +11,17 @@ root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 conf.read(root_path + "/config.ini")
 
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    # execute all other hooks to obtain the report object
-    outcome = yield
-    rep = outcome.get_result()
-
-    # we only look at actual failing test calls, not setup/teardown
-    if rep.when == "call" and rep.failed:
-        mode = "a" if os.path.exists("failures") else "w"
-        with open("failures", mode) as f:
-            f.write(rep.longreprtext + "\n")
+# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     # execute all other hooks to obtain the report object
+#     outcome = yield
+#     rep = outcome.get_result()
+#
+#     # we only look at actual failing test calls, not setup/teardown
+#     if rep.when == "call" and rep.failed:
+#         mode = "a" if os.path.exists("failures") else "w"
+#         with open("failures", mode) as f:
+#             f.write(rep.longreprtext + "\n")
 
 
 def _get_default_connection_args():
