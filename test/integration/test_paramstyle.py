32a17d889e66e02df30f689b7949bcb4c91046e9
Brooke White <whbrook@amazon.com>
2021-12-06 09:25:18 -0800

    test(paramstyle): add integration tests, rework

---
+++ test/integration/test_paramstyle.py
@@ -0,0 +1,127 @@
+import typing
+
+import pytest  # type: ignore
+
+
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        ({"c1": "abc", "c2": "defg", "c3": "hijkl"}, ["abc", "defg", "hijkl"]),
+        ({"c1": "a", "c2": "b", "c3": "c"}, ["a", "b", "c"]),
+    ],
+)
+def test_pyformat(cursor, parameters):
+    cursor.paramstyle = "pyformat"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_pyformat(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.execute("insert into test_pyformat(c1, c2, c3) values(%(c1)s, %(c2)s, %(c3)s)", data)
+    cursor.execute("select * from test_pyformat")
+    res: typing.Tuple[typing.List[str, str, str]] = cursor.fetchall()
+    assert len(res) == 1
+    assert len(res[0]) == len(data)
+    assert res[0] == exp_result
+
+
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        (
+            ({"c1": "abc", "c2": "defg", "c3": "hijkl"}, {"c1": "a", "c2": "b", "c3": "c"}),
+            [["abc", "defg", "hijkl"], ["a", "b", "c"]],
+        ),
+    ],
+)
+def test_pyformat_multiple_insert(cursor, parameters):
+    cursor.paramstyle = "pyformat"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_pyformat(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.executemany("insert into test_pyformat(c1, c2, c3) values(%(c1)s, %(c2)s, %(c3)s)", data)
+    cursor.execute("select * from test_pyformat")
+    res: typing.Tuple[typing.List[str, str, str], ...] = cursor.fetchall()
+    assert len(res) == len(exp_result)
+    for idx, row in enumerate(res):
+        assert len(row) == len(exp_result[idx])
+        assert row == exp_result[idx]
+
+
+@pytest.mark.parametrize(
+    "parameters", [(["abc", "defg", "hijkl"], ["abc", "defg", "hijkl"]), (["a", "b", "c"], ["a", "b", "c"])]
+)
+def test_qmark(cursor, parameters):
+    cursor.paramstyle = "qmark"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_qmark(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.execute("insert into test_qmark(c1, c2, c3) values(?, ?, ?)", data)
+    cursor.execute("select * from test_qmark")
+    res: typing.Tuple[typing.List[str, str, str]] = cursor.fetchall()
+    assert len(res) == 1
+    assert len(res[0]) == len(data)
+    assert res[0] == exp_result
+
+
+@pytest.mark.parametrize(
+    "parameters", [(["abc", "defg", "hijkl"], ["abc", "defg", "hijkl"]), (["a", "b", "c"], ["a", "b", "c"])]
+)
+def test_numeric(cursor, parameters):
+    cursor.paramstyle = "numeric"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_numeric(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.execute("insert into test_numeric(c1, c2, c3) values(:1, :2, :3)", data)
+    cursor.execute("select * from test_numeric")
+    res: typing.Tuple[typing.List[str, str, str]] = cursor.fetchall()
+    assert len(res) == 1
+    assert len(res[0]) == len(data)
+    assert res[0] == exp_result
+
+
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        ({"parameter1": "abc", "parameter2": "defg", "parameter3": "hijkl"}, ["abc", "defg", "hijkl"]),
+        ({"parameter1": "a", "parameter2": "b", "parameter3": "c"}, ["a", "b", "c"]),
+    ],
+)
+def test_named(cursor, parameters):
+    cursor.paramstyle = "named"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_named(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.execute("insert into test_named(c1, c2, c3) values(:parameter1, :parameter2, :parameter3)", data)
+    cursor.execute("select * from test_named")
+    res: typing.Tuple[typing.List[str, str, str]] = cursor.fetchall()
+    assert len(res) == 1
+    assert len(res[0]) == len(data)
+    assert res[0] == exp_result
+
+
+@pytest.mark.parametrize(
+    "parameters", [(["abc", "defg", "hijkl"], ["abc", "defg", "hijkl"]), (["a", "b", "c"], ["a", "b", "c"])]
+)
+def test_format(cursor, parameters):
+    cursor.paramstyle = "format"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_format(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.execute("insert into test_format(c1, c2, c3) values(%s, %s, %s)", data)
+    cursor.execute("select * from test_format")
+    res: typing.Tuple[typing.List[str, str, str]] = cursor.fetchall()
+    assert len(res) == 1
+    assert len(res[0]) == len(data)
+    assert res[0] == exp_result
+
+
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        ([["abc", "defg", "hijkl"], ["a", "b", "c"]], [["abc", "defg", "hijkl"], ["a", "b", "c"]]),
+    ],
+)
+def test_format_multiple(cursor, parameters):
+    cursor.paramstyle = "format"
+    data, exp_result = parameters
+    cursor.execute("create temporary table test_format(c1 varchar, c2 varchar, c3 varchar)")
+    cursor.executemany("insert into test_format(c1, c2, c3) values(%s, %s, %s)", data)
+    cursor.execute("select * from test_format")
+    res: typing.Tuple[typing.List[str, str, str], ...] = cursor.fetchall()
+    assert len(res) == len(exp_result)
+    for idx, row in enumerate(res):
+        assert len(row) == len(exp_result[idx])
+        assert row == exp_result[idx]
--- test/unit/test_paramstyle.py
+++ test/unit/test_paramstyle.py
@@ -6,77 +6,73 @@ from redshift_connector.core import convert_paramstyle as convert
 
 
 def test_qmark():
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert(
-        "qmark", 'SELECT ?, ?, "field_?" FROM t ' "WHERE a='say ''what?''' AND b=? AND c=E'?\\'test\\'?'", args
+    new_query, make_args = convert(
+        "qmark", 'SELECT ?, ?, "field_?" FROM t ' "WHERE a='say ''what?''' AND b=? AND c=E'?\\'test\\'?'"
     )
     expected: str = 'SELECT $1, $2, "field_?" FROM t WHERE ' "a='say ''what?''' AND b=$3 AND c=E'?\\'test\\'?'"
-    assert (new_query, vals) == (expected, args)
+    assert new_query == expected
+    assert make_args((1, 2, 3)) == (1, 2, 3)
 
 
 def test_qmark_2():
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert("qmark", "SELECT ?, ?, * FROM t WHERE a=? AND b='are you ''sure?'", args)
+    new_query, make_args = convert("qmark", "SELECT ?, ?, * FROM t WHERE a=? AND b='are you ''sure?'")
     expected: str = "SELECT $1, $2, * FROM t WHERE a=$3 AND b='are you ''sure?'"
-    assert (new_query, vals) == (expected, args)
+    assert new_query == expected
+    assert make_args((1, 2, 3)) == (1, 2, 3)
 
 
 def test_numeric():
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert("numeric", "SELECT sum(x)::decimal(5, 2) :2, :1, * FROM t WHERE a=:3", args)
+    new_query, make_args = convert("numeric", "SELECT sum(x)::decimal(5, 2) :2, :1, * FROM t WHERE a=:3")
     expected: str = "SELECT sum(x)::decimal(5, 2) $2, $1, * FROM t WHERE a=$3"
-    assert (new_query, vals) == (expected, args)
+    assert new_query == expected
+    assert make_args((1, 2, 3)) == (1, 2, 3)
 
 
 def test_numeric_default_parameter():
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert("numeric", "make_interval(days := 10)", args)
+    new_query, make_args = convert("numeric", "make_interval(days := 10)")
 
-    assert (new_query, vals) == ("make_interval(days := 10)", args)
+    assert new_query == "make_interval(days := 10)"
+    assert make_args((1, 2, 3)) == (1, 2, 3)
 
 
 def test_named():
-    args: typing.Dict[str, int] = {
-        "f_2": 1,
-        "f1": 2,
-    }
-    new_query, vals = convert("named", "SELECT sum(x)::decimal(5, 2) :f_2, :f1 FROM t WHERE a=:f_2", args)
+    new_query, make_args = convert("named", "SELECT sum(x)::decimal(5, 2) :f_2, :f1 FROM t WHERE a=:f_2")
     expected: str = "SELECT sum(x)::decimal(5, 2) $1, $2 FROM t WHERE a=$1"
-    assert (new_query, vals) == (expected, (1, 2))
+    assert new_query == expected
+    assert make_args({"f_2": 1, "f1": 2}) == (1, 2)
 
 
 def test_format():
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert(
+    new_query, make_args = convert(
         "format",
         "SELECT %s, %s, \"f1_%%\", E'txt_%%' FROM t WHERE a=%s AND b='75%%' AND c = '%' -- Comment with %",
-        args,
     )
-    expected: str = "SELECT $1, $2, \"f1_%%\", E'txt_%%' FROM t WHERE a=$3 AND b='75%%' AND c = '%' -- Comment with %"
-    assert (new_query, vals) == (expected, args)
+    expected: str = (
+        "SELECT $1, $2, \"f1_%%\", E'txt_%%' FROM t WHERE a=$3 AND " "b='75%%' AND c = '%' -- Comment with %"
+    )
+    assert new_query == expected
+    assert make_args((1, 2, 3)) == (1, 2, 3)
 
     sql: str = r"""COMMENT ON TABLE test_schema.comment_test """ r"""IS 'the test % '' " \ table comment'"""
-    new_query, vals = convert("format", sql, args)
-    assert (new_query, vals) == (sql, args)
+    new_query, make_args = convert("format", sql)
+    assert new_query == sql
 
 
 def test_format_multiline():
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert("format", "SELECT -- Comment\n%s FROM t", args)
-    assert (new_query, vals) == ("SELECT -- Comment\n$1 FROM t", args)
+    new_query, make_args = convert("format", "SELECT -- Comment\n%s FROM t")
+    assert new_query == "SELECT -- Comment\n$1 FROM t"
 
 
 def test_py_format():
-    args: typing.Dict[str, int] = {"f2": 1, "f1": 2, "f3": 3}
-
-    new_query, vals = convert(
-        "pyformat", "SELECT %(f2)s, %(f1)s, \"f1_%%\", E'txt_%%' FROM t WHERE a=%(f2)s AND b='75%%'", args
+    new_query, make_args = convert(
+        "pyformat", "SELECT %(f2)s, %(f1)s, \"f1_%%\", E'txt_%%' " "FROM t WHERE a=%(f2)s AND b='75%%'"
     )
-    expected: str = "SELECT $1, $2, \"f1_%%\", E'txt_%%' FROM t WHERE a=$1 AND b='75%%'"
-    assert (new_query, vals) == (expected, (1, 2))
+    expected: str = "SELECT $1, $2, \"f1_%%\", E'txt_%%' FROM t WHERE a=$1 AND " "b='75%%'"
+    assert new_query == expected
+    assert make_args({"f2": 1, "f1": 2, "f3": 3}) == (1, 2)
 
     # pyformat should support %s and an array, too:
-    args: typing.Tuple[int, int, int] = (1, 2, 3)
-    new_query, vals = convert("pyformat", "SELECT %s, %s, \"f1_%%\", E'txt_%%' FROM t WHERE a=%s AND b='75%%'", args)
-    expected: str = "SELECT $1, $2, \"f1_%%\", E'txt_%%' FROM t WHERE a=$3 AND b='75%%'"
-    assert (new_query, vals) == (expected, args)
+    new_query, make_args = convert("pyformat", "SELECT %s, %s, \"f1_%%\", E'txt_%%' " "FROM t WHERE a=%s AND b='75%%'")
+    expected = "SELECT $1, $2, \"f1_%%\", E'txt_%%' FROM t WHERE a=$3 AND " "b='75%%'"
+    assert new_query, expected
+    assert make_args((1, 2, 3)) == (1, 2, 3)
