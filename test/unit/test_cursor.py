9c0726375bcbd16b98bad5605db99a60f5eaa7fd
yash621 <yg17381@gmail.com>
2022-01-21 21:14:34 +0530

    removed umcompleted tests

--- test/unit/test_cursor.py
+++ test/unit/test_cursor.py
@@ -251,15 +251,10 @@ def test_get_tables_considers_args(is_single_database_metadata_val, _input, sche
             assert arg in spy.call_args[0][1]
 
 
-@pytest.mark.parametrize("filename",)
-@pytest.mark.parametrize("table_name",)
-@pytest.mark.parametrize("column_indexes",)
-@pytest.mark.parametrize("delimeter",)
-@pytest.mark.parametrize("column_names",)
-def test_insert_data_bulk(filename,table_name, column_indexes,column_names,delimeter,mocker):
-    
 
     
+
+
 
 
 
