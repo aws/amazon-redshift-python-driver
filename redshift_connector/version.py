4a10732a5550fc5a711a1818ac16d36a19f6746a
Bram Neijt <bram@neijt.nl>
2022-09-08 20:15:00 +0200

    Update gitignore

--- redshift_connector/version.py
+++ redshift_connector/version.py
@@ -2,4 +2,4 @@
 # and read it at runtime using importlib_metadata for python < 3.7
 from importlib_metadata import version
 
-__version__ = version(__name__)
+__version__ = version(__name__.split(".")[0])
