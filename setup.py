26fc02dd860a31daf31b92b4ccf1ef66a09f3cdf
James Dow <42390019+jamesdow21@users.noreply.github.com>
2024-06-27 19:19:04 -0400

    chore: publish inline type annotations (#224)
    Allow inline type hints to be packaged and distributed
    following PEP561 specification
    https://peps.python.org/pep-0561/#specification
    
    Co-authored-by: James Dow <JAMES.DOW@GAPAC.COM>

--- setup.py
+++ setup.py
@@ -128,7 +128,9 @@ setup(
     ],
     keywords="redshift dbapi",
     include_package_data=True,
-    package_data={"redshift-connector": ["*.py", "*.crt", "LICENSE", "NOTICE"]},
+    package_data={
+        "redshift-connector": ["*.py", "*.crt", "LICENSE", "NOTICE", "py.typed"]
+    },
     packages=find_packages(exclude=["test*"]),
     cmdclass=custom_cmds,
 )
